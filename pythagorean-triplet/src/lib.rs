e5fbd129b4ca3d9acd8dfcff68b10749cbf922de
aguestuser <aguestuser@riseup.net>
2019-03-02 12:04:19 -0500

    [pythagorean] incorporate mentor feedback to pythagorean triplets ex

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -15,25 +15,18 @@ use std::collections::HashSet;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     (1_u32..(sum / 3_u32))
         .into_par_iter()
-        .map(|a| {
-            let (b, remainder) = calculate_b_and_remainder(a, sum - a, sum);
+        .filter_map(|a| {
+            let (b, remainder) = calculate_b_and_remainder(a, sum - a);
             let c = sum - a - b;
-
-            if is_valid(a, b, c, remainder) {
-                Some([a, b, c])
-            } else {
-                None
+            match remainder == 0 && a < b {
+                true => Some([a, b, c]),
+                false => None,
             }
         })
-        .filter_map(|x| x)
-        .collect()
+        .collect::<HashSet<[u32; 3]>>()
 }
 
-fn calculate_b_and_remainder(a: u32, b_plus_c: u32, sum: u32) -> (u32, u32) {
+fn calculate_b_and_remainder(a: u32, b_plus_c: u32) -> (u32, u32) {
     let (numerator, denominator) = ((b_plus_c).pow(2) - a.pow(2), 2 * b_plus_c);
     (numerator / denominator, numerator % denominator)
-}
-
-fn is_valid(a: u32, b: u32, c: u32, remainder: u32) -> bool {
-    remainder == 0 && a < b && b < c
 }
